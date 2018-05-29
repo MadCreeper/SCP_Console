0e1439e98d2d611a4434e1064db88c4d15d2d864
MadCreeper <33979935+MadCreeper@users.noreply.github.com>
2018-05-29 23:02:11 +0800

    更加函数式样！5.29
    加入了一些新功能

--- load.py
+++ load.py
@@ -1,21 +1,34 @@
 import  urllib.request
 import re
 import os
-url = r'http://scp-wiki-cn.wikidot.com/'
-scpid = input()
-url += scpid
-res = urllib.request.urlopen(url)
-html = res.read().decode('utf-8')
+def load_scp_database(scpid):
+    url = r'http://scp-wiki-cn.wikidot.com/'
+    url += scpid
+    res = urllib.request.urlopen(url)
+    html = res.read().decode('utf-8')
+    scplog = open(logpath+scpid+'.html','w',encoding='utf-8')
+    scplog.write(html)
+
+    dr = re.compile(r'<[^>]+>',re.S)
+    dd = dr.sub('',html)
+    print(dd)
+def batch_load(l,r):
+    for i in range(l,r+1):
+        load_scp_database('scp-'+str(i))
+
+#url = r'http://scp-wiki-cn.wikidot.com/'
+#scpid = input()
+#url += scpid
+#res = urllib.request.urlopen(url)
+#html = res.read().decode('utf-8')
 #print(html)
-
-dr = re.compile(r'<[^>]+>',re.S)
-dd = dr.sub('',html)
-print(dd)
-
+#dr = re.compile(r'<[^>]+>',re.S)
+#dd = dr.sub('',html)
+#print(dd)
 
 logpath='D:/scp_log/'
 if not os.path.exists(logpath):
     os.mkdir(logpath)
-scplog = open(logpath+scpid+'.html','w',encoding='utf-8')
-scplog.write(html)
-scplog.close()
+batch_load(1000,1100)
+scp_id=input()
+load_scp_database(scp_id)
--- scp.py
+++ scp.py
@@ -2,20 +2,33 @@ import  urllib.request
 import re
 import os
 logpath='D:/scp_log/'
+welcome_word = "欢迎登陆SCiPNET直接存储终端。请输入口令"
+FAIL_READ = 'READ_FAILED'
 def create_log_folder(): # creates folder for the logs
     
     if not os.path.exists(logpath): 
         os.mkdir(logpath)
 
 def get_scp(scpid): # gets SCP file from Internet
-    url = r'http://scp-wiki-cn.wikidot.com/'
+    url = 'http://scp-wiki-cn.wikidot.com/'
     url += scpid
-    res = urllib.request.urlopen(url)
-    html = res.read().decode('utf-8')
-    return html
-   
+    try:
+        res = urllib.request.urlopen(url)
+        html = res.read().decode('utf-8')
+        return html
+    except:
+        print(scpid + 'Load FAILED!')
+        return FAIL_READ    
+    else:
+        print(scpid + 'Load Success')
+    
+    
+def batch_load(l,r):
+    for i in range(l,r+1):
+        get_scp('scp-'+str(i))
+
 def output_raw_scp_file(html): # Output the main files, ignore the rest
-    dr = re.compile(r'<[^>]+>',re.S)
+    
     file_begin = '<div id="page-content">'
     file_end =   '<div class="page-tags">'
     pos1 = int(html.find(file_begin))
@@ -23,8 +36,11 @@ def output_raw_scp_file(html): # Output the main files, ignore the rest
     #print(pos1)
     #print(pos2)
     html = html[pos1:pos2]
-    dd = dr.sub('',html)
-    print(dd)
+
+    dr = re.compile(r'<[^>]+>',re.S)
+    raw_document = dr.sub('',html)
+    raw_document = raw_document.strip()
+    print(raw_document)
 
 def record_log(scpid,html):
     scplog = open(logpath+scpid+'.html','w',encoding='utf-8')
@@ -33,13 +49,25 @@ def record_log(scpid,html):
 
 #main:
 create_log_folder()
+print(welcome_word)
 while(1):
     inputLine = input() + ' '
+
     if(inputLine.find('access')==0):
         scp_id = inputLine[len('access')+1:inputLine.find(' ',len('access')+2)]
-        print('Accessing'+scp_id+'...')
-        
-        scp_html=get_scp(scp_id)
-        output_raw_scp_file(scp_html)
-        record_log(scp_id,scp_html)
+        print('Accessing '+scp_id+' ...')
+        print('------------------------------')
 
+        print(scp_id.upper())
+        scp_html=get_scp(scp_id)
+        if(scp_html==FAIL_READ):
+            print('This SCP does not exist or denied access!')
+        else:
+            output_raw_scp_file(scp_html)
+            record_log(scp_id,scp_html)
+
+        print('------------------------------')
+
+    if(inputLine.find('exit')==0):
+        print("Exiting System.")
+        break
